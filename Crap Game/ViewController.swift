c9e414e5780d153ac1e696fde4ea2de4bac05603
João Victor Ferreira Pimenta <joaovictorfp30@gmail.com>
2021-06-02 13:46:52 -0300

    Initial Commit

---
+++ Crap Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crap Game
+//
+//  Created by João Victor Ferreira Pimenta on 02/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
